5a8f0d18edc91824c877d08168f8d687d8b23517
gayan madurapperuma <madurapperumamags@gmail.com>
2018-03-19 03:35:46 +0530

    jwt implemt

--- db/db.js
+++ db/db.js
@@ -1,10 +1,11 @@
 const Sequelize = require('sequelize');
+const jwt = require('jsonwebtoken');
 //https://github.com/gayanMadurapperuma/AuthO.git
 //127.0.0.1
 //root
 //root
 //testAuth
-const db = new Sequelize('testAuth', 'root', 'root', {
+const db = new Sequelize('AuthTest', 'root', 'root', {
     host : '127.0.0.1',
     dialect : 'mysql',
     operatorsAliases: false,
@@ -16,7 +17,7 @@ const db = new Sequelize('testAuth', 'root', 'root', {
     }
 });
 
-const user = db.define('user', {
+var user = db.define('user', {
     name : {
         type : Sequelize.STRING,
         allowNull : false,
@@ -39,10 +40,95 @@ const user = db.define('user', {
             notEmpty : true
         }
     }
-})
+},{
+    getterMethods : {
+        fullName : function () {
+            return this.name + ' fullName'
+        }
+    }
+    // instanceMethods : {
+    //     generateAuthToken : function() {
+    //        //user = this;
+    //        console.log('Inside the function')
+    //        //return 'Hello';
+    //        ////console.log('GenerateAuthToken',user); 
+    //     }
+    // }
+});
 
+user.prototype.getAuth = function(){
+    //console.log(this.user.);
+    const token = jwt.sign({id : this.id, access : 'auth'}, 'abc123');
+    // Authentication.create({
+    //     access : 'auth',
+    //     token : token,
+    //     userId : this.id 
+    // }.then(auth => {
+    //     console.log(auth)
+    //     //return auth;
+    // }))
+    return token;
+    //return 
+    //return this.id;
+}
+
+var school = db.define('school', {
+    name : {
+        type : Sequelize.STRING,
+        allowNull : false,
+        validate : {
+            notEmpty : true
+        }
+    },
+    courses : {
+        type : Sequelize.STRING,
+        allowNull : false,
+        validate : {
+            notEmpty : true
+        }
+    }
+});
+
+var Authentication = db.define('Authentication', {
+    access : {
+       type : Sequelize.STRING,
+       allowNull : false,
+       validate : {
+           notEmpty : true
+       } 
+    },
+    token : {
+       type : Sequelize.STRING,
+       allowNull : false,
+       validate : {
+           notEmpty : true
+       } 
+    }
+}, {
+    instanceMethods : {
+
+    },
+    classMethods : {
+
+    }
+});
+
+//user.belongsToMany(school);
+user.hasMany(Authentication);
+//Authentication.belongsToMany(user, {through : user});
+// user.create({
+//     name : 'gayan2',
+//     username : 'gayan2@gmail.com',
+//     password : '123123'
+// });
+
+// school.setuser({schoolId : 1}).then((sc) => {
+//     console.log(sc);
+// })
 //exports module 
 module.exports = {
     db : db,
-    user : user
+    user : user,
+    school : school,
+    auth : Authentication
 }
--- user/user.js
+++ user/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {user} = require('../db/db');
+const {user,school,auth} = require('../db/db');
 
 router.get('/', (req, res) => {
     user.findAll().then(users => {
@@ -19,5 +19,84 @@ router.post('/add', (req,res) => {
         res.status(400).send('bad request ' + e);
     })
 })
+
+router.post('/school', (req, res) => {
+    school.create(req.body).then(sch => {
+        res.send(sch);
+    }).catch(e => {
+        res.status(400).send('bad request ' + e);
+    }) 
+})
+
+router.put('/:id/school', (req,res) => {
+    user.findById(req.params.id).then(fountUser => {
+        if (!fountUser){
+            return res.status(404);
+        }
+        return fountUser.setschool(req.body.schoolId)
+        //console.log(JSON.stringify(fountUser,undefined,2))})
+    }).then(console.log(JSON.stringify(res,undefined,2)))
+    // }).then(res.send(res)).catch(e => {
+    //     res.status(400).send('bad request ' + e);
+    // })
+})
+
+router.get('/:id', (req, res) => {
+    user.findById(req.params.id).then(foundUser => {
+        if (!foundUser)
+            return res.status(404).send();
+        //console.log(foundUser);
+        console.log('GenAuth',foundUser.getAuth());
+        auth.create({
+            access : 'auth',
+            token : foundUser.getAuth(),
+            userId : foundUser.id 
+        }).then(token => {
+            return res.header('x-auth', token.token).send(foundUser);
+        })     
+    }).catch(e => {
+        console.log(e);
+        return res.status(400).send(e);
+    })
+})
+
+router.get('/get', (req,res) => {
+    //console.log('authGet');
+    auth.findAll().then(auths => {
+        if (!auths)
+            return res.status(404).send('not found auth')
+        return res.send(auths);
+    }).catch(e => {
+        res.status(400).send(e);
+    })
+    // auth.findAll().then(getAuth => {
+    //     return res.send(getAuth);
+    // }).catch(e => {
+    //     console.log(e);
+    //     res.status(400).send();
+    // })
+    
+})
+
+router.post('/auth', (req,res)=> {
+    auth.create({
+                access : 'auth',
+                token : 'this' 
+    }).then(token => {
+        res.send(token);
+    }).catch(e => {
+        res.status(400).send();
+    })
+    // auth.create({
+    //     access : 'auth',
+    //     token : 'this' 
+    // }.then(token => {
+    //     return res.send(token);
+    // }));
+})
+
+router.post('/login', (req,res) => {
+    
+});
 
 module.exports = router;
